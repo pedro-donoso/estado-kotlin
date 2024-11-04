86a07216f5a9502e1f727f007f3bfaf68784c33e
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-04 15:12:47 -0300

    agrego padding

--- app/src/main/java/com/example/estado/MainScreen.kt
+++ app/src/main/java/com/example/estado/MainScreen.kt
@@ -1,12 +1,13 @@
 package com.example.estado
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -17,29 +18,31 @@ import com.example.estado.main.BookViewModel
 fun MainScreen(bookViewModel: BookViewModel) {
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
         items(bookViewModel.books.value) { book ->
-            Column(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)) {
+            // Contenedor para cada libro
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically // Alinear verticalmente
+            ) {
+                // Imagen del libro
                 Image(
                     painter = rememberAsyncImagePainter(book.coverUrl),
                     contentDescription = "Cover of ${book.title}",
                     modifier = Modifier
-                        .width(100.dp)
+                        .width(150.dp)
                         .height(200.dp)
-                        .fillMaxWidth()
-                        .padding(bottom = 8.dp)
+                        .padding(end = 16.dp) // Espacio a la derecha de la imagen
                 )
-                Text(text = book.title)
-                Text(text = book.author)
-                Spacer(modifier = Modifier.height(8.dp))
 
-                // Divisor
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(1.dp)
-                        .background(Color.Black)
-                )
-                Spacer(modifier = Modifier.height(4.dp))
+                // Contenedor para el título y el autor
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = book.title, modifier = Modifier.padding(bottom = 4.dp))
+                    Text(text = book.author)
+                }
             }
+            // Divisor entre libros
+            Divider(color = Color.Gray, thickness = 1.dp)
         }
     }
 }
--- app/src/main/java/com/example/estado/main/BookViewModel.kt
+++ app/src/main/java/com/example/estado/main/BookViewModel.kt
@@ -24,13 +24,13 @@ class BookViewModel : ViewModel() {
                     "Pride and Prejudice",
                     "Jane Austen",
                     1813,
-                    "https://th.bing.com/th/id/OIP.rX3bivDFwC-271y5wMlIrQHaLO?rs=1&pid=ImgDetMain"
+                    "https://th.bing.com/th/id/OIP.17fuccisPdTvLIHxK2hcPgHaLU?rs=1&pid=ImgDetMain"
                 ),
                 Book(
                     "The Catcher in the Rye",
                     "J.D. Salinger",
                     1951,
-                    "https://th.bing.com/th/id/OIP.rX3bivDFwC-271y5wMlIrQHaLO?rs=1&pid=ImgDetMain"
+                    "https://idunneditorial.com/wp-content/uploads/2016/04/El-guardia%CC%81n-entre-el-centeno.jpg"
                 )
                 )
         }
