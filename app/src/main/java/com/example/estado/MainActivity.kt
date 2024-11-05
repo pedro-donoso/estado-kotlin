3361046978d47cbbdccde05b23cef6daa28055b6
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-05 11:31:09 -0300

    cambio AppCompatActivity en MainActivity.kt

--- app/src/main/java/com/example/estado/MainActivity.kt
+++ app/src/main/java/com/example/estado/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.estado
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.example.estado.main.BookViewModel
 import com.example.estado.main.MainScreen
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bookViewModel by viewModels<BookViewModel>()
